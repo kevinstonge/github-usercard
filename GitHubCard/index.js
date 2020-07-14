9d019716798b49995f13496c24674e20dfea4092
Kevin St.Onge <kevinstonge@gmail.com>
2020-07-14 13:01:55 -0400

    axios working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,11 @@
+const { default: axios } = require("axios");
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get("https://api.github.com/users/kevinstonge").then(r=>console.log(r)).catch(err=>console.log(err))
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
